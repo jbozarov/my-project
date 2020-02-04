6f9fb37856d1c307003b7c86f855045556e70478
John Bozarov <codinginusa@gmail.com>
2020-02-03 22:05:24 -0700

    Calculator added

---
+++ src/components/calculator/Buttons.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+export class Buttons extends Component {
+
+  buttonPressed = e => {
+    this.props.buttonPressed(e.target.name)
+  }
+  render() {
+    return (
+      <div>
+      <div className='container' >
+      <div className='row'>
+            <button name='√' onClick={this.buttonPressed} >√</button>
+            <button name='x2' onClick={this.buttonPressed} >x2</button>
+            <button name='CE' onClick={this.buttonPressed} >CE</button>
+            <button name='C' onClick={this.buttonPressed} >C</button>
+      </div>
+      <div className='row' >
+            <button name='7' onClick={this.buttonPressed} >7</button>
+            <button name='8' onClick={this.buttonPressed} >8</button>
+            <button name='9' onClick={this.buttonPressed} >9</button>
+            <button name='/' onClick={this.buttonPressed} >:</button>
+      </div>
+      <div className='row' >
+            <button name='4' onClick={this.buttonPressed} >4</button>
+            <button name='5' onClick={this.buttonPressed} >5</button>
+            <button name='6' onClick={this.buttonPressed} >6</button>
+            <button name='*' onClick={this.buttonPressed} >X</button>
+      </div>
+      <div className='row' >
+            <button name='1' onClick={this.buttonPressed} >1</button>
+            <button name='2' onClick={this.buttonPressed} >2</button>
+            <button name='3' onClick={this.buttonPressed} >3</button>
+            <button name='-' onClick={this.buttonPressed} >-</button>
+      </div>
+      <div className='row' >
+            <button name='0' onClick={this.buttonPressed} >0</button>
+            <button name='.' onClick={this.buttonPressed} >.</button>
+            <button name='+' onClick={this.buttonPressed} >+</button>
+            <button name='=' onClick={this.buttonPressed} >=</button>
+      </div>
+  </div>
+      </div>
+    )
+  }
+}
+
+export default Buttons
--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Buttons from './Buttons';
+import './Calculator.css'
 
-export class Calculator extends Component {
-    render() {
-        return (
-            <div>
-                Calculator
-            </div>
-        )
+class Calculator extends Component {
+  constructor () {
+    super (); 
+    this.state = { results: ''}
+  }
+
+
+
+  buttonPressed = buttonName => {
+    if (buttonName==='=') this.calculate() 
+    else if (buttonName==='C') this.reset(); 
+    else if (buttonName==='√') this.sqrt(); 
+    else if (buttonName==='CE') this.backspace(); 
+    else if (buttonName==='x2') this.powed(); 
+    else this.setState({results: this.state.results+buttonName})
+  } 
+
+  calculate = () => {
+    try {
+      let result = eval(this.state.results); 
+      if (Number.isInteger(result)===false) this.setState ({results: +result.toFixed(2)})
+      else this.setState ({results:  result})
     }
-}
+    catch {
+      this.setState ({results: 'error'})
+    }
+  }
 
-export default Calculator
+  reset = () => this.setState ({results: ''})
+  sqrt = () => this.setState ({results: Math.sqrt(this.state.results)})
+  backspace = () => this.setState ({results: this.state.results.slice(0, this.state.results.length-1)})
+  powed = () => this.setState ({results: Math.pow(this.state.results, 2)})
+
+  render () {
+    const {results} = this.state
+    return (
+      <div className="calculator">
+      <h1 className='calc-header' >Welcome to calculator </h1>
+      <div className='screen' ></div>{results} 
+      <Buttons buttonPressed={this.buttonPressed} /> 
+      </div>
+      );
+    }
+  }
+
+export default Calculator;
